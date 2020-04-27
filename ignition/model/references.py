545cfc6fdcd996933e3dcbce41053b0cfc9ec168
Daniel Vaccaro-Senna <daniel.vaccaro-senna@accantosystems.com>
2020-04-27 17:11:05 +0100

    Include resourceId property on find references response

--- ignition/model/references.py
+++ ignition/model/references.py
@@ -9,18 +9,20 @@ class FindReferenceResponse():
 
 class FindReferenceResult():
 
-    def __init__(self, associated_topology=None, outputs=None):
+    def __init__(self, resource_id, associated_topology=None, outputs=None):
+        self.resource_id = resource_id
         self.associated_topology = associated_topology if associated_topology is not None else []
         self.outputs = outputs if outputs is not None else []
 
     def __str__(self):
-        return f'associated_topology: {self.associated_topology} outputs: {self.outputs}'
+        return f'resource_id: {self.resource_id} associated_topology: {self.associated_topology} outputs: {self.outputs}'
 
 
 def find_reference_response_dict(find_reference_response):
     result = None
     if find_reference_response.result is not None:
         result = {
+            'resource_id': find_reference_response.result.resource_id,
             'associatedTopology': find_reference_response.result.associated_topology,
             'outputs': find_reference_response.result.outputs
         }
