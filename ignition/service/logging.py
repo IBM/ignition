3ebcc513112baa375f49665bd25ad93d32dbd2e7
Daniel Vaccaro-Senna <daniel.vaccaro-senna@ibm.com>
2021-06-24 11:19:16 +0100

    Lowercase x-tracectx

--- ignition/service/logging.py
+++ ignition/service/logging.py
@@ -20,7 +20,7 @@ PRIVATE_KEY_SUFFIX = '-----END RSA PRIVATE KEY-----'
 PRIVATE_KEY_REGEX = re.compile('{0}(.*?){1}'.format(PRIVATE_KEY_PREFIX, PRIVATE_KEY_SUFFIX), flags=re.DOTALL)
 OBFUSCATED_PRIVATE_KEY = '***obfuscated private key***'
 
-LM_HTTP_HEADER_PREFIX = "X-Tracectx-"
+LM_HTTP_HEADER_PREFIX = "x-tracectx-"
 LOGGING_CONTEXT_KEY_PREFIX = "tracectx."
 LM_HTTP_HEADER_TXNID = "TransactionId".lower()
 LM_HTTP_HEADER_PROCESS_ID = "ProcessId".lower()
